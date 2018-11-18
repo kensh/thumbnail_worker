dca52d7b20bc17acc8595361b07009ed45ee0d93
kensuke shimokawa <kenngkenng.gml@gmail.com>
2018-11-18 11:09:47 +0900

    download png format

--- imagemagick.js
+++ imagemagick.js
@@ -4,7 +4,7 @@ const path = require('path');
 const dir = path.join(__dirname, 'upload/');
 
 const convert = (filename) => {
-  imagemagick.convert([dir + filename, '-resize', '100x100', dir + 'thumbnail_' + filename], (err, stdout) => {
+  imagemagick.convert([dir + filename, '-resize', '100x100', dir + 'thumbnail_' + filename + ".png"], (err, stdout) => {
     if (err) throw err;
     console.log('stdout:', stdout);
   });
